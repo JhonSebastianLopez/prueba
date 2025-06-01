7a562ec13b892f8c7c1cd258aae4a918ff1b74ca
Jhon Lopez <jhonlv10@gmail.com>
2025-06-01 13:02:48 -0500

    seguridad enablescuriry

--- src/main/java/com/example/demo/SeguridadWeb.java
+++ src/main/java/com/example/demo/SeguridadWeb.java
@@ -3,9 +3,12 @@ package com.example.demo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SeguridadWeb {
 
     @Bean
@@ -32,6 +35,11 @@ public class SeguridadWeb {
                 .disable());
 
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
 
